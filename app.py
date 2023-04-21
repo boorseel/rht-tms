8835e1dd350bd28fec69e71740543df4de5ac6e2
Ubuntu <ubuntu@ip-172-31-23-180.eu-central-1.compute.internal>
2023-04-21 12:38:13 +0000

    commitments still

--- app.py
+++ app.py
@@ -12,6 +12,32 @@ import random
 import string
 from flask_mail import Mail, Message
 from sqlalchemy.sql import text
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import declarative_base, Session
+
+#Debuging
+import sentry_sdk
+sentry_sdk.init(
+            dsn="https://e577e42812b045689b535c6a52337dc4@o4505041920065536.ingest.sentry.io/4505041925439488",
+            traces_sample_rate=1.0
+            )
+
+#OS SETTINGS
+os.chdir('/var/www/rht-tms')
+
+def create_directory_if_not_exists(directory_path):
+    if not os.path.exists(directory_path):
+        os.makedirs(directory_path)
+
+def initialize_directories():
+    required_directories = [
+            'barcodes',
+            'exports'
+    ]
+    for directory in required_directories:
+        create_directory_if_not_exists(directory)
+
+
 
 #MAIL SETTINGS
 app = Flask(__name__)
@@ -29,6 +55,20 @@ db = SQLAlchemy(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 
+#Event model
+class Event(db.Model):
+        event_ID = db.Column(db.String(6), primary_key=True)
+        event_Name = db.Column(db.String(255), nullable=False)
+        event_Date = db.Column(db.Date, nullable=False)
+        
+#Ticket model
+Base = declarative_base()
+class Ticket(Base):
+    __tablename__ = 'tickets'
+    ticket_ID = Column(String, primary_key=True)
+    event_ID = Column(Integer)
+
+
 #User model
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -38,28 +78,88 @@ class User(UserMixin, db.Model):
     role = db.Column(db.String(255), nullable=False)
     @classmethod
     def get_by_id(cls, user_id):
-        return cls.query.get(int(user_id))
+        return cls.session.get(int(user_id))
         
 #User loader
 @login_manager.user_loader
 def load_user(user_id):
     return User.get_by_id(user_id)
+#Log out
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('You have successfully logged out.')
+    return redirect(url_for('login'))
+
+#BASE 
+@app.route('/') 
+@login_required 
+def index():
+    if current_user.role in ('admin', 'super_user'):
+        return redirect(url_for('admin_main')) 
+    elif current_user.role == 'seller': 
+        return redirect(url_for('seller_main'))   
+    return redirect(url_for('login')) 
+
 
 #Login function
+#@app.route('/login', methods=['GET', 'POST'])
+#def login():
+#    if current_user.is_authenticated:
+#        return redirect(url_for('index'))
+#    if request.method == 'POST':
+#        username = request.form['username']
+#        password = hashlib.sha256(request.form['password'].encode()).hexdigest()
+#        user = User.query.filter_by(username=username).first()
+#        if user and user.password == password:
+#            login_user(user)
+#            return redirect(url_for('index'))
+#        else:
+#            flash('Invalid credentials.')
+#            return render_template('login.html')
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
+        print("User is already authenticated")
         return redirect(url_for('index'))
     if request.method == 'POST':
         username = request.form['username']
         password = hashlib.sha256(request.form['password'].encode()).hexdigest()
         user = User.query.filter_by(username=username).first()
+
+        if user:
+            print(f"Expected password: {user.password}, provided password: {password}")
+
         if user and user.password == password:
             login_user(user)
+            print("User logged in successfully")
             return redirect(url_for('index'))
         else:
             flash('Invalid credentials.')
+            print("Invalid credentials")
             return render_template('login.html')
+    else:
+        return render_template('login.html')
+
+#Admin main landing
+@app.route('/admin_main')
+@login_required
+def admin_main():
+    if current_user.role not in ('admin', 'super_user'):
+        flash('You do not have permission to access this page.')
+        return redirect(url_for('index'))
+    return render_template('admin_main.html')
+
+#Seller main landing
+@app.route('/seller_main')
+@login_required
+def seller_main():
+    if current_user.role == 'seller':
+        return render_template('seller_main.html')
+
+
 
 #Create user
 @app.route('/create_user', methods=['GET', 'POST'])
@@ -88,17 +188,17 @@ def create_event():
        flash('You do not have permission to access this page.') 
        return redirect(url_for('index')) 
    if request.method == 'POST': 
-        event_name = request.form['event_name'] 
-        event_date = request.form['event_date'] 
-        event_date_obj = datetime.strptime(event_date, '%Y-%m-%d') 
-        unique_hash = hashlib.sha1(f"{event_name}{event_date}".encode('utf-8')).hexdigest()[:10]
-        new_event = Event(event_ID=unique_hash, event_name=event_name, event_date=event_date_obj) 
+        event_Name = request.form['event_Name'] 
+        event_Date = request.form['event_Date'] 
+        event_Date_obj = datetime.strptime(event_Date, '%Y-%m-%d') 
+        unique_hash = hashlib.sha1(f"{event_Name}{event_Date}".encode('utf-8')).hexdigest()[:6]
+        new_event = Event(event_ID=unique_hash, event_Name=event_Name, event_Date=event_Date_obj) 
         db.session.add(new_event) 
         db.session.commit() 
         flash('Event successfully created.') 
    return render_template('create_event.html')   
 
-#SEARCH EVEN   
+#SEARCH EVENT 
 @app.route('/search_events', methods=['GET', 'POST']) 
 @login_required 
 def search_events(): 
@@ -106,46 +206,64 @@ def search_events():
     filtered_events = events 
     if request.method == 'POST': 
         search_term = request.form['search_term']
-        event_date = request.form['event_date']
-        filtered_events = [event for event in events if (search_term.lower) in event.event_name.lower()] and (event_date == '' or event.event_date == datetime.strptime(event_date, '%Y-%m-%d'))
+        event_Date = request.form['event_Date']
+        filtered_events = [event for event in events if (search_term.lower) in event.event_Name.lower()] and (event_Date == '' or event.event_Date == datetime.strptime(event_Date, '%Y-%m-%d'))
         return render_template('search_events.html', events=filtered_event)
 
 #GENERATE TICKET ID   
-@app.route('/generate_tickets', methods=['GET', 'POST']) 
-@login_required 
-def generate_tickets(): 
-    if current_user.role not in ('admin', 'super_user'): 
-        flash('You do not have permission to access this page.') 
-        return redirect(url_for('index'))  
-    events = Event.query.all()   
-    if request.method == 'POST': 
-        event_id = request.form['event_id'] 
-        num_tickets = int(request.form['num_tickets']) 
-        event = Event.query.filter_by(event_ID=event_id).first() 
-        ticket_ids = []   
-    for _ in range(num_tickets):
-        ticket_id = ''.join(random.choices(string.ascii_uppercase + string.digits, k=10)) 
-        ticket = Ticket(ticket_ID=ticket_id, event_id=event.id) 
-        db.session.add(ticket) 
-        db.session.commit()
+@app.route('/generate_tickets', methods=['GET', 'POST'])
+@login_required
+def generate_tickets():
+    if current_user.role not in ('admin', 'super_user'):
+        flash('You do not have permission to access this page.')
+        return redirect(url_for('index'))
+    events = Event.query.all()
+    if request.method == 'POST':
+        event_ID = None
+        try:
+            event_ID = request.form['event_ID']
+        except KeyError:
+            flash('Event ID not provided.', 'error')
+            return redirect(url_for('index'))
+        event = Event.query.filter_by(event_ID=event_ID).first()
+        if event is not None:
+            ticket_ids = []
+            flash('Event found.', 'info')
 
-        #Generate barcode 
-        ean = barcode.get('ean13', ticket_id, writer=ImageWriter()) 
-        filename = f"barcodes/{ticket_id}.png" 
-        ean.save(filename)   
-        ticket_ids.append(ticket_id)
-         
-    # Export ticket IDs to Excel 
-    wb = Workbook() 
-    ws = wb.active 
-    ws.title = f"Tickets for {event.event_name}" 
-    ws.append(['Ticket ID'])  
-    for ticket_id in ticket_ids: 
-        ws.append([ticket_id])   
-        wb.save(f"exports/tickets_{event.event_name}.xlsx")
-        flash(f"{num_tickets} ticket IDs successfully generated for {event.event_name}.") 
-        return send_file(f"exports/tickets_{event.event_name}.xlsx", as_attachment=True) 
-    return render_template('generate_tickets.html', events=events)  
+            print("Starting ticket generation...")
+            
+            for _ in range(num_tickets):
+                ticket_id = ''.join(random.choices(string.ascii_uppercase + string.digits, k=10))
+                ticket = Ticket(ticket_ID=ticket_id, event_ID=event.ID)
+                db.session.add(ticket)
+                db.session.commit()
+                # Generate barcode
+                ean = barcode.get('ean13', ticket_ID, writer=ImageWriter())
+                filename = f"barcodes/{ticket_id}.png"
+                ean.save(filename)
+                ticket_ids.append(ticket_id)
+                
+                print("Generating barcode for ticket:", ticket_id)
+                ean.save(filename)
+                print("Barcode saved:", filename)
+
+
+            flash('Tickets generated successfully!')
+            # Export ticket IDs to Excel
+            wb = Workbook()
+            ws = wb.active
+            ws.title = f"Tickets for {event.event_Name}"
+            ws.append(['Ticket ID'])
+            for ticket_id in ticket_ids:
+                ws.append([ticket_id])
+            wb.save(f"exports/tickets_{event.event_Name}.xlsx")
+            flash(f"{num_tickets} ticket IDs successfully generated for {event.event_Name}.")
+            return send_file(f"exports/tickets_{event.event_Name}.xlsx", as_attachment=True)
+        else:
+            flash("Event not found", "error")
+            return redirect(url_for('index'))
+    return render_template('generate_tickets.html', events=events)
+
 
 #ACTIVATE TICKET   
 @app.route('/activate_ticket', methods=['GET', 'POST']) 
@@ -166,11 +284,11 @@ def activate_ticket():
         flash('Ticket successfully activated.') 
   
         #Send email confirmation to the student 
-        event = Event.query.get(ticket.event_id) 
+        event = Event.session.get(ticket.event_IDi) 
         msg = Message('Ticket Activation Confirmation',
                       sender='your_email@example.com', 
                       recipients=[student.student_EMAIL])
-        msg.body = f"Dear {student.student_NAME} {student.student_SNAME},\n\nYour ticket for {event.event_name} on {event.event_date.strftime('%Y-%m-%d')} has been successfully activated. Please keep this email for your records.\n\nBest regards,\nRadio HighTECH" 
+        msg.body = f"Dear {student.student_NAME} {student.student_SNAME},\n\nYour ticket for {event.event_Name} on {event.event_Date.strftime('%Y-%m-%d')} has been successfully activated. Please keep this email for your records.\n\nBest regards,\nRadio HighTECH" 
         mail.send(msg) 
     else:
         flash('Ticket or student not found. Please check the information and try again.') 
@@ -187,9 +305,9 @@ def refund_ticket():
     if request.method == 'POST': 
         ticket_id = request.form['ticket_id'] 
         ticket = Ticket.query.filter_by(ticket_ID=ticket_id).first() 
-        event_id = ticket.event_id 
-        active_tickets_table = f"active_tickets_{event_id}" 
-        refund_tickets_table = f"refund_tickets_{event_id}"   
+        event_ID = ticket.event_ID 
+        active_tickets_table = f"active_tickets_{event_ID}" 
+        refund_tickets_table = f"refund_tickets_{event_ID}"   
     if ticket: 
         
         #Move ticket from active_tickets to refund_tickets
@@ -216,18 +334,18 @@ def analytics():
     events = Event.query.all()   
     event_analytics = [] 
     for event in events: 
-        event_id = event.event_ID 
-        active_tickets_table = f"active_tickets_{event_id}" 
-        refund_tickets_table = f"refund_tickets_{event_id}" 
-        spent_tickets_table = f"spent_tickets_{event_id}"  
+        event_ID = event.event_ID 
+        active_tickets_table = f"active_tickets_{event_ID}" 
+        refund_tickets_table = f"refund_tickets_{event_ID}" 
+        spent_tickets_table = f"spent_tickets_{event_ID}"  
         
         #Calculate KPIs for the event 
         active_tickets_count = db.engine.execute(text(f"SELECT COUNT(*) FROM {active_tickets_table}")).scalar()
         refund_tickets_count = db.engine.execute(text(f"SELECT COUNT(*) FROM {refund_tickets_table}")).scalar()
         spent_tickets_count = db.engine.execute(text(f"SELECT COUNT(*) FROM {spent_tickets_table}")).scalar()   
         event_analytics.append({ 
-            'event_name': event.event_name, 
-            'event_date': event.event_date, 
+            'event_Name': event.event_Name, 
+            'event_Date': event.event_Date, 
             'active_tickets': active_tickets_count, 
             'refund_tickets': refund_tickets_count, 
             'spent_tickets': spent_tickets_count 
@@ -245,9 +363,9 @@ def validate_ticket():
     if request.method == 'POST': 
         ticket_id = request.form['ticket_id'] 
         ticket = Ticket.query.filter_by(ticket_ID=ticket_id).first() 
-        event_id = ticket.event_id 
-        active_tickets_table = f"active_tickets_{event_id}" 
-        spent_tickets_table = f"spent_tickets_{event_id}" 
+        event_ID = ticket.event_ID 
+        active_tickets_table = f"active_tickets_{event_ID}" 
+        spent_tickets_table = f"spent_tickets_{event_ID}" 
         if ticket: 
             student_id = ticket.student_id 
             student_data = Student.query.filter_by(student_ID=student_id).first() 
@@ -269,3 +387,8 @@ def validate_ticket():
              
 if __name__ == "__main__": 
     app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(ssl_context="adhoc")
+    initialize_directories()
+    #Comment out after initial run.
+    with app.app_context():
+        db.create_all()
