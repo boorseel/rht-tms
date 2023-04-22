a2cb4d1f7595836671b767edd98436d522b7654e
boorseel <99136367+boorseel@users.noreply.github.com>
2023-04-22 14:09:43 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, flash, redirect, url_for
-from flask_sqlalchemy import SQLAlchemy, 
+from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 from datetime import datetime
 import hashlib
